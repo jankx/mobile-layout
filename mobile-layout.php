f18e8a8100563ffc40c4099e29e2468ba44efee9
Puleeno Nguyen <puleeno@gmail.com>
2020-02-16 17:30:57 +0700

    Install dependences for mobile layout

--- mobile-layout.php
+++ mobile-layout.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * Plugin Name: Jankx Mobile Layout
+ * Plugin URI: https://puleeno.com
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Create mobile
+ */
+
